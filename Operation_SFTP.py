34a704704374a10a8113f90300202349122e6d49
Hank Chen <hank@pixis.com.tw>
2025-04-23 15:39:18 +0800

    fix

--- Operation_SFTP.py
+++ Operation_SFTP.py
@@ -1,13 +1,18 @@
-import paramiko,csv
-from scp import SCPClient
+import paramiko,csv,os
 
-def UploadFolder_by_SFTP(ProbeIP,account,pwd,local_folder,remote_folder) -> None:
+def UploadFolder_by_SFTP(ProbeIP,account,pwd,Local_Path,Remote_Path) -> None:
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(hostname=ProbeIP, username= account,password= pwd)
-            with SCPClient(ssh.get_transport()) as scp:
-                scp.put(local_folder, recursive=True, remote_path=remote_folder)
+            sftp = ssh.open_sftp()
+            ssh.exec_command(f'rm -rf {Remote_Path}') #預先刪除遠端的資料夾，以免重複
+            if os.path.isdir(Local_Path):
+                sftp.mkdir(Remote_Path)
+                for item in os.listdir(Local_Path):
+                    sftp.put(localpath=f'{Local_Path}\\{item}',remotepath=f'{Remote_Path}/{item}')
+            else:
+                 sftp.put(Local_Path,Remote_Path)
             ssh.close()
             print(f'{ProbeIP} has been finished to upload')
 
@@ -19,8 +24,8 @@ if __name__ == "__main__":
 
     SShacc = input('SSH Account: ')
     SSHpwd = input('SSH Password: ')
-    local_folder = input('Please type localPath: ')
-    remote_folder = input('Please type RemotePath: ')
+    local_path = input('Please type localPath: ')
+    remote_path = input('Please type RemotePath: ')
     ProbeDaemonIPs = []
     print('load data IPlist.csv')
     with open('IPlist.csv',mode = 'r',encoding='utf-8') as f:
@@ -28,5 +33,5 @@ if __name__ == "__main__":
         next(tempdataDaemon)
         ProbeDaemonIPs = list(tempdataDaemon)
     for ip in ProbeDaemonIPs :
-        UploadFolder_by_SFTP(ProbeIP=ip[0],account=SShacc,pwd=SSHpwd,local_folder=local_folder,remote_folder=remote_folder)
+        UploadFolder_by_SFTP(ProbeIP=ip[0],account=SShacc,pwd=SSHpwd,Local_Path=local_path,Remote_Path=remote_path)
     input('key any key to exit')
